f1c61dd78c85c9762e983b993c7e4f49453e54d2
PremAnbu <premanbu9360@gmail.com>
2024-02-07 13:13:40 +0530

    Created Simple Linked List

---
+++ ListMainClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes
+{
+    internal class ListMainClass
+    {
+        static void Main()
+        {
+            LinkedList<int> li=new LinkedList<int>();
+            li.AddFirst(56);
+            li.AddFirst(30);
+            li.AddFirst(70);
+            //SinglyLinkedList l=new SinglyLinkedList ();
+            //l.Add(56);
+            //l.Add(30);
+            //l.Add(70);
+            //l.Display();
+        }
+    }
+}
