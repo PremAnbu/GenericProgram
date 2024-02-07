eeab5a2a8a2da0a01352c10ec9f517b8cfcb97f8
PremAnbu <premanbu9360@gmail.com>
2024-02-07 13:24:42 +0530

    Created Custom LinkedList

--- ListMainClass.cs
+++ ListMainClass.cs
@@ -10,15 +10,11 @@ namespace GenericTypes
     {
         static void Main()
         {
-            LinkedList<int> li=new LinkedList<int>();
-            li.AddFirst(56);
-            li.AddFirst(30);
-            li.AddFirst(70);
-            //SinglyLinkedList l=new SinglyLinkedList ();
-            //l.Add(56);
-            //l.Add(30);
-            //l.Add(70);
-            //l.Display();
+            SinglyLinkedList l=new SinglyLinkedList ();
+            l.Add(56);
+            l.Add(30);
+            l.Add(70);
+            l.Display();
         }
     }
 }
